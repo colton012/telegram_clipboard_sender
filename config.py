55ba176ae0c34e2ba8d9146a43d4444c2bc547e4
colton012 <83549892+colton012@users.noreply.github.com>
2023-09-15 19:37:52 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+TOKEN = '6438802307:AAHqekf0cC1YhFAy6uM5by3lTdZ-oWjlGbk' #токен бота
+USERID = '672105492' #chatid
+
+#USERID чата узнать тут (пункт Your user ID: ****)- https://t.me/getmyid_bot
+#TOKEN бота найти тут - https://t.me/BotFather 
